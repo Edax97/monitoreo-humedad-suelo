48c851bd8f7325714bf87662509cf819d5a4f6ed
Edwin Campos <ecamposc@uni.pe>
2023-06-21 11:10:50 -0500

    Minor fix

--- src/components/common/grafico/GraficoCard.tsx
+++ src/components/common/grafico/GraficoCard.tsx
@@ -7,10 +7,10 @@ interface Props {
 export default function GraficoCard(props: Props) {
   return (
     <div
-      className={`card shadow px-2 px-lg-3 py-2 ${props.className}`}
+      className={`card shadow px-2 px-lg-3 py-2 m-auto ${props.className}`}
       style={{ backgroundColor: "var(--bs-gray-200)" }}
     >
-      <div style={{ overflowX: "auto", maxWidth: "90vw", margin: "auto" }}>
+      <div style={{ overflowX: "auto", maxWidth: "90vw" }}>
         <div style={{ minWidth: 700 }}>{props.children}</div>
       </div>
     </div>
